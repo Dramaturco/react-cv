d2c9e8f122447660c8a7b3eaa4d2ba96a5362f98
Emre Neumann <emre.neumann@crownpeak.com>
2023-06-03 13:31:08 +0200

    added expansion logic to language entries

--- src/FactsAndSkills/LanguageEntry.js
+++ src/FactsAndSkills/LanguageEntry.js
@@ -1,15 +1,24 @@
-import {Rating} from "../Generic";
+import { Rating } from "../Generic";
+import { useState, Fragment } from "react";
 
 const LanguageEntry = ({ name, fluency, comments }) => {
+  //const [expanded, setExpanded] = useState(false);
+  const expanded = false;
   return (
-    <div className="language-box">
-      {name}
-      <Rating value={fluency} max={5}/>
-      <div className="language-comment-box no-print">
-        {comments.map((comment) => (
-          <span key={comment} className="language-comment">{comment}</span>
-        ))}
-      </div>
+    <div className={`language-box${expanded ? " expanded" : ""}`} onClick={() => setExpanded(!expanded)}>
+      <span className="language-name">{name}</span>
+      {expanded && (
+        <Fragment>
+          <Rating value={fluency} max={5} />
+          <div className="language-comment-box no-print">
+            {comments.map((comment) => (
+              <span key={comment} className="language-comment">
+                {comment}
+              </span>
+            ))}
+          </div>
+        </Fragment>
+      )}
     </div>
   );
 };
